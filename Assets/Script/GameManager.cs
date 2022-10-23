2bd8f19c74946c3279fa9a26e8422b5c4d8a0deb
rzkyapriil <rzkyapriil@gmail.com>
2022-10-24 00:54:50 +0700

    update

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Player player;
     // public GameObject SettingsMenu;
     [SerializeField] GameObject grass;
     [SerializeField] GameObject road;
@@ -31,6 +32,7 @@ public class GameManager : MonoBehaviour
             //instantiative blocknya
             CreateTerrain(prefab, z);
         }
+        player.SetUp(minZPos, extent);
     }
 
     private void CreateTerrain(GameObject prefab, int zPos)
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,53 +7,75 @@ public class Player : MonoBehaviour
 {
     [SerializeField, Range(0.01f, 1f)] float moveDuration = 0.2f;
     [SerializeField, Range(0.01f, 1f)] float jumpHeight = 0.8f;
+    private int minZPos;
+    private int extent;
+    private float backBoundary;
+    private float leftBoundary;
+    private float rightBoundary;
+
+    public void SetUp(int minZPos, int extent)
+    {
+        backBoundary = minZPos-1;
+        leftBoundary = -(extent+1);
+        rightBoundary = extent+1;
+    }
 
     private void Update()
     {
         var moveDir = Vector3.zero;
-        var rotateDir = Vector3.zero;
+        // var rotateDir = Vector3.zero;
 
         if(Input.GetKey(KeyCode.UpArrow))
         {
             moveDir += new Vector3(0, 0, 1);
-            rotateDir = new Vector3(0f, 0f, 0f);
+            // rotateDir = new Vector3(0f, 0f, 0f);
         }
         
         if(Input.GetKey(KeyCode.DownArrow))
         {
             moveDir += new Vector3(0, 0, -1);
-            rotateDir = new Vector3(0f, 180f, 0f);
+            // rotateDir = new Vector3(0f, 180f, 0f);
         }
 
         if(Input.GetKey(KeyCode.RightArrow))
         {
             moveDir += new Vector3(1, 0, 0);
-            rotateDir = new Vector3(0f, 90f, 0f);
+            // rotateDir = new Vector3(0f, 90f, 0f);
         }
         
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             moveDir += new Vector3(-1, 0, 0);
-            rotateDir = new Vector3(0f, -90f, 0f);
+            // rotateDir = new Vector3(0f, -90f, 0f);
         }
 
         if(moveDir != Vector3.zero && isJumping() == false)
-            Jump(moveDir, rotateDir);
+            Jump(moveDir);
     }
 
-    private void Jump(Vector3 targetMoveDirection, Vector3 targetRotateDirection)
+    private void Jump(Vector3 targetDirection)
     {
-        var TargetPosition = transform.position + targetMoveDirection;
+        // Atur Posisi
+        var targetPosition = transform.position + targetDirection;
+        transform.LookAt(targetPosition);
 
-        //transform.LookAt(targetDirection);
-
+        // Loncat keatas
         var moveSeq = DOTween.Sequence(transform);
         moveSeq.Append(transform.DOMoveY(jumpHeight, moveDuration/2));
-        moveSeq.Append(transform.DOMoveY(0.4f, moveDuration/2));
+        moveSeq.Append(transform.DOMoveY(0, moveDuration/2));
 
-        transform.DOMoveX(TargetPosition.x, moveDuration);
-        transform.DOMoveZ(TargetPosition.z, moveDuration);
-        transform.DORotate(targetRotateDirection, moveDuration/2);
+        if(targetPosition.z <= backBoundary ||
+            targetPosition.x <= leftBoundary ||
+            targetPosition.x >= rightBoundary)
+            return;
+
+        if(Tree.AllPositions.Contains(targetPosition))
+            return;
+        
+        // gerak maju/mundur/samping
+        transform.DOMoveX(targetPosition.x, moveDuration);
+        transform.DOMoveZ(targetPosition.z, moveDuration);
+        // transform.DORotate(targetRotateDirection, moveDuration/2);
     }
 
     private bool isJumping()
@@ -72,9 +94,9 @@ public class Player : MonoBehaviour
 
     private void AnimateDie()
     {
-        transform.DOScaleY(0.01f, 1);
-        transform.DOScaleX(0.25f, 1);
-        transform.DOScaleZ(0.2f, 1);
+        transform.DOScaleY(0.1f, 1);
+        transform.DOScaleX(2, 1);
+        transform.DOScaleZ(2, 1);
         this.enabled = false;
     }
 
